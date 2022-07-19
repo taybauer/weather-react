9982e9f9d7cdd4f67ba611548bbfab99c6361934
taybauer <102003686+taybauer@users.noreply.github.com>
2022-07-19 18:07:49 -0500

    Updated components with weather app JS files
    installed axios

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,12 @@
 import React from "react";
+import Search from "./Search";
+import "./App.css";
 
-export default function Weather() {
-  return <h2>Hello from Weather</h2>;
+export default function App() {
+  return (
+    <div className="SearchEngine">
+      <h1>Weather Search Engine</h1>
+      <Search />
+    </div>
+  );
 }
