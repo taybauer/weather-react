3df831fd18a53c794c43b13c9d3df10658c468c3
taybauer <102003686+taybauer@users.noreply.github.com>
2022-07-23 16:00:59 -0500

    Added forecast, dates and icon package

--- src/index.js
+++ src/index.js
@@ -1,54 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
-import Weather from "./Weather";
-import reportWebVitals from "./reportWebVitals";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <div className="App">
-    <div className="container">
-      <div>
-        <Weather />
-      </div>
-      <h1>New York</h1>
-      <ul>
-        <li>Wednesday 07:00</li>
-        <li>Mostly Cloudy</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
-            alt="Raining"
-          />
-          27° C
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 15%</li>
-            <li>Humidity: 72%</li>
-            <li>Wind: 20 km/h</li>
-          </ul>
-        </div>
-      </div>
-      <p>
-        This project was coded by Taylor Bauer and is{" "}
-        <a
-          href="https://github.com/taybauer/weather-react"
-          alt="GitHub Repository"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>
-      </p>
-    </div>
-  </div>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById("root"));
